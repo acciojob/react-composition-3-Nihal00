a57c917e628b7272244372db8f9feac350e1d868
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-11 18:22:58 +0000

    one

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
+
+  const [isShow, setIsShow] = useState(false);
+
+  const onHover = () => setIsShow(true);
+  const offHover = () => setIsShow(false);
+
   return (
-    <div>
+    <div className="tootiptext">
         {/* Do not remove the main div */}
+        {isShow ? <Tooltip 
+          text= "This is a tooltip" 
+        /> : ""}
+        <h1 
+          onMouseEnter={onHover}
+          onMouseLeave={offHover}
+        >Hover over me</h1>
+        <hr />
+        {isShow ? <Tooltip 
+          text="This is another tooltip"
+        /> : ""}
+        <p
+          onMouseEnter={onHover}
+          onMouseLeave={offHover}
+        >Hover over me to see the another tooltip</p>
+        <hr />
     </div>
   )
 }
