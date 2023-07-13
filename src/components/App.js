23d2fdc1746dc7cd76546671e4febd15c67b3e09
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-07-13 11:34:04 +0000

    third

--- src/components/App.js
+++ src/components/App.js
@@ -6,17 +6,23 @@ import Tooltip from "./Tooltip";
 const App = () => {
 
   const [state, setState] = useState(false);
+  const [stateTool, setToolState] = useState(false);
 
     return(
       <div>
         <div className="tootip" onMouseEnter={() => setState(true)} onMouseLeave={() => setState(false)}>
-          <h1>Hover over me</h1>
+          
           {state ? <Tooltip text="This is tooltip" /> : ""}
+
+          <h1>Hover over me</h1>
+
         </div>
 
-        <div className="tooltiptext" onMouseEnter={() => setState(true)} onMouseLeave={() => setState(false)} >
+        <div className="tooltiptext" onMouseEnter={() => setToolState(true)} onMouseLeave={() => setToolState(false)} >
+         
+          {stateTool ? <Tooltip text="This is another tooltip" /> : ""}
+
           <p>Hover over me to see another tooltip</p>
-          {state ? <Tooltip text="This is another tooltip" /> : ""}
         </div>
 
       </div>
